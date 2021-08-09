2ffa7424a1fccb24c285822694f78305698f97e5
Andrey Gorbunov <andrey@optimuspro.ru>
2021-08-09 16:28:59 +0300

    Refs #24958: Yandex cloud client - implement ability to fetch speakers list and prepare the speaker for further use

--- yandex_station/station_client_cloud.py
+++ yandex_station/station_client_cloud.py
@@ -22,7 +22,7 @@ class SyncCloudClient:
     session: ClientSession 
     yandex: YandexSession
     loop: AbstractEventLoop
-
+    
     def __init__(self): 
         self.loop = asyncio.get_event_loop()
         self.session = ClientSession(loop = self.loop)
@@ -30,6 +30,34 @@ class SyncCloudClient:
     def start(self):
         self.loop.run_forever()
 
+    def get_speakers(self, token: str) -> List[SpeakerConfig]:
+        r = self.__get_speakers_async(token)
+        return asyncio.run_coroutine_threadsafe(r, self.loop).result()
+
+    async def __get_speakers_async(self, token: str) -> List[SpeakerConfig]:
+        quasar = await self.__get_quasar(token)
+        await quasar.load_devices()
+
+        return [
+            SpeakerConfig(item['id'], item['name'], item['scenario_id'])
+            for item in quasar.speakers
+        ]
+
+    def prepare_speaker(self, token: str, speaker: SpeakerConfig) -> SpeakerConfig:
+        r = self.__prepare_speaker_async(token, speaker)
+        return asyncio.run_coroutine_threadsafe(r, self.loop).result()
+
+    async def __prepare_speaker_async(self, token: str, speaker: SpeakerConfig) -> SpeakerConfig:
+        quasar = await self.__get_quasar(token)
+        speaker_data = self.__convert_speaker_config_to_quasar_object(speaker)
+        
+        await quasar.prepare_speaker(speaker_data)
+        
+        return SpeakerConfig(
+            speaker_data['id'], 
+            speaker_data['name'], 
+            speaker_data['scenario_id']
+        )
 
     def say(self, token: str, device: SpeakerConfig, phrase: str):
         r = self.say_async(token, device, phrase)
